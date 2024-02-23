2292e17ea13518252e94c506e5732da1d2763643
Saul Goodman <1rn21cs016.akash@gmail.com>
2024-02-23 23:10:03 +0530

    fixed login bug

--- server/views.py
+++ server/views.py
@@ -8,9 +8,9 @@ from django.shortcuts import get_object_or_404
 @api_view(['POST'])
 def login(request):
     user=get_object_or_404(User,username=request.data['username'])
-    if not user.check_password(request.data['password']):
+    if user.check_password(request.data['password']):
         return Response({"detail":"Invalid password or password"},status=status.HTTP_400_BAD_REQUEST)
-    token,created=Token.objects.get_or_creaet(user=user)
+    token,created=Token.objects.get_or_create(user=user)
     serializer=UserSerializer(instance=user)
     return Response({"token":token.key,"user":serializer.data})
 @api_view(['POST'])
